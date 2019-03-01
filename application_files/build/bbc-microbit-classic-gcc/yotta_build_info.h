8dbcf85cd4e12408eb211783744686b58167493f
Paul Goddard <paul2.goddard@live.uwe.ac.uk>
2019-03-01 13:21:32 +0000

    Finished readme

--- application_files/build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ application_files/build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -2,10 +2,10 @@
 #define __YOTTA_BUILD_INFO_H__
 // yotta build info, #include YOTTA_BUILD_INFO_HEADER to access
 #define YOTTA_BUILD_YEAR 2019 // UTC year
-#define YOTTA_BUILD_MONTH 2 // UTC month 1-12
-#define YOTTA_BUILD_DAY 27 // UTC day 1-31
-#define YOTTA_BUILD_HOUR 16 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 5 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 8 // UTC second 0-61
-#define YOTTA_BUILD_UUID 5091c38d-b8f0-45ba-9a19-56035f65192a // unique random UUID for each build
+#define YOTTA_BUILD_MONTH 3 // UTC month 1-12
+#define YOTTA_BUILD_DAY 1 // UTC day 1-31
+#define YOTTA_BUILD_HOUR 13 // UTC hour 0-24
+#define YOTTA_BUILD_MINUTE 3 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 33 // UTC second 0-61
+#define YOTTA_BUILD_UUID 98bba8bb-c2ba-478c-b7b5-360cc72302c7 // unique random UUID for each build
 #endif // ndef __YOTTA_BUILD_INFO_H__
